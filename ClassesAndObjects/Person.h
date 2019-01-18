fa659e944deb7bc6a1535bf42d2fe751ddf7b344
Bill <billiamspub@gmail.com>
2019-01-17 18:10:18 -0800

    Add project files.

---
+++ ClassesAndObjects/Person.h
@@ -0,0 +1,14 @@
+#include <string>
+class Person
+{
+private:
+	std::string firstname;
+	std::string lastname;
+	int arbitrarynumber;
+	
+public:
+	Person(std::string first, std::string last, int arbitrary);
+	Person() = default; // default keyword can also be used for other constructors
+	std::string getName();
+};
+
